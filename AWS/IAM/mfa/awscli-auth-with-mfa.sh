9c818f0edb39b9fcce3b88421b7a0f7b82bb914e
Kobi <kobi@spinningops.com>
2023-02-04 18:32:45 +0200

    check if jq installed

--- AWS/IAM/mfa/awscli-auth-with-mfa.sh
+++ AWS/IAM/mfa/awscli-auth-with-mfa.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
-set -e
+# this script is creating temp keys for use with awscli and mfa profile
+### to export the vars start the script like this
+# . ./awscli-auth-with-mfa.sh
 
+# check if jq is installed
+REQUIRED_PACKAGE="jq"
+PACKAGE_OK=$(dpkg -s $REQUIRED_PACKAGE|grep "install ok installed")
+echo Checking for $REQUIRED_PACKAGE: $PACKAGE_OK
+if [ "" = "$PACKAGE_OK" ]; then
+  echo "No $REQUIRED_PACKAGE. Please install jq"
+  echo "jq is a tool for processing JSON inputs"
+  return
+fi
+
+# export the default profile to generate tokens
+# if you have multiple users modify it instead of default
+export AWS_PROFILE=default
+
+# input
 echo "Hi, Please add your AWS account id"
 read ACCOUNT_ID
 
 echo "Please add your IAM username"
 read MFA_USER
 
-echo "Please add your 6-digit code from google authenticator"
+echo "Please add your 6-digit code from your authenticator app"
 read MFA_TOKEN
 
 TOKEN=$(aws sts get-session-token --duration-seconds 3600 --serial-number arn:aws:iam::$ACCOUNT_ID:mfa/$MFA_USER --token-code $MFA_TOKEN)
@@ -27,6 +44,9 @@ aws configure set aws_session_token $TEMP_TOKEN --profile mfa
 
 # cleanup
 rm temp-token.json
+
+# export mfa as default
+export AWS_PROFILE=mfa
 
 echo "Access token is ready! If you need to switch AWS_PROFILE use the script awscli-switch-aws-profile.sh"
 echo "default profile is "$AWS_PROFILE
